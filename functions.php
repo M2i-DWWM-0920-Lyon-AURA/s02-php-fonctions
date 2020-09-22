edc5db2b45c3240168513ffc2742a1a5ecf2ae89
Romain Tello <romain.tello@gmail.com>
2020-09-22 15:50:30 +0200

    Solve doubleArray function

--- functions.php
+++ functions.php
@@ -7,3 +7,17 @@ function double(int $number): int {
 function times(int $number1, int $number2): int {
     return $number1 * $number2;
 }
+
+function doubleArray(array $array): array {
+    // Créer un nouveau tableau
+    $result = [];
+    // Pour chaque élément du tableau reçu en paramètre
+    foreach ($array as $number) {
+        // Multiplier cet élément par 2
+        $number = $number * 2;
+        // Stocker le résultat dans le nouveau tableau
+        array_push($result, $number);
+    }
+    // Renvoyer le nouveau tableau
+    return $result;
+}
